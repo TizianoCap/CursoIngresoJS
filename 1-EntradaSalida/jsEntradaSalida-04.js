d316ea717ec2d1ac6652ed87f20dca0230273244
TizianoCap <tizicapuani@gmail.com>
2021-02-02 18:21:15 -0300

    prueba

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,13 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombre;
+// guardo en la variable nombre el texto que escribio en ususario dentro de la ventana pront
+	nombre = prompt("ingrese su nombre");
+//copio en nombre que tengo guardado en la variable nombre dentro de la caja de texto embebida en la pagina
+	document.getElementById("txtIdNombre").nodeValue = nombre;
+	
+	
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -3,6 +3,11 @@ Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados
 ej.: "Usted se llama José y tiene 66 años" 	*/
 function mostrar()
 {	
-	alert("ok");
+	let nombre
+	let edad
+
+	nombre = document.getElementById("txtIdNombre").Value;
+	edad = document.getElementById("txtIdEdad").Value;
+	alert("usted se llama " + nombre + " y tiene " + edad + " años");
 }
 
